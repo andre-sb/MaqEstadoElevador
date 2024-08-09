c98a3f2888338a35f2808d9d0a564f10ccc07410
André Braga <asbraga@hotmail.com>
2024-08-09 17:54:48 -0400

    Aceleração e desaceleração

--- elevador.py
+++ elevador.py
@@ -16,27 +16,56 @@ class Elevador:
 
     def PararMotor(self):
         self.direcao.write_text('0')
+        self.tempo.write_text('0.2')        
 
     def MotorSubir(self):
         self.direcao.write_text('-1')
+        self.tempo.write_text('0.2')
 
     def MotorDescer(self):
         self.direcao.write_text('1')
+        self.tempo.write_text('0.2')
+
+    def MotorAceleraSobe(self):
+        self.direcao.write_text('-1')
+        self.tempo.write_text('0.8')
+
+    def MotorFreiaSobe(self):
+        self.direcao.write_text('-1')
+        self.tempo.write_text('0.8')
+
+    def MotorAceleraDesce(self):
+        self.direcao.write_text('1')
+        self.tempo.write_text('0.8')
+
+    def MotorFreiaDesce(self):
+        self.direcao.write_text('1')
+        self.tempo.write_text('0.8')
 
 
 estados = [
     State('inicio'),
-    State('parado'      , on_enter='PararMotor'),
-    State('subindo'     , on_enter='MotorSubir'),
-    State('descendo'    , on_enter='MotorDescer'),
+    State('parado'        , on_enter='PararMotor'),
+    State('subindo'       , on_enter='MotorSubir'),
+    State('acelera_sobe'  , on_enter='MotorAceleraSobe'),
+    State('freia_sobe'    , on_enter='MotorFreiaSobe'),    
+    State('descendo'      , on_enter='MotorDescer'),
+    State('acelera_desce' , on_enter='MotorAceleraDesce'),
+    State('freia_desce'   , on_enter='MotorFreiaDesce')
     ]
 
 transicoes = [
     {'source':'inicio',        'trigger':'iniciar',    'dest':'parado'         },
-    {'source':'parado',        'trigger':'subir',      'dest':'subindo'        },
-    {'source':'parado',        'trigger':'descer',     'dest':'descendo'       },
-    {'source':'subindo',       'trigger':'chegou',     'dest':'parado'         },
-    {'source':'descendo',      'trigger':'chegou',     'dest':'parado'         },
+
+    {'source':'parado',        'trigger':'subir',      'dest':'acelera_sobe'   },
+    {'source':'acelera_sobe',  'trigger':'veloc',      'dest':'subindo'        },
+    {'source':'subindo',       'trigger':'perto',      'dest':'freia_sobe'     },
+    {'source':'freia_sobe',    'trigger':'chegou',     'dest':'parado'         },
+
+    {'source':'parado',        'trigger':'descer',     'dest':'acelera_desce'  },
+    {'source':'acelera_desce', 'trigger':'veloc',      'dest':'descendo'       },
+    {'source':'descendo',      'trigger':'perto',      'dest':'freia_desce'    },
+    {'source':'freia_desce',   'trigger':'chegou',     'dest':'parado'         },
 ]
 
 elevador = Elevador()
