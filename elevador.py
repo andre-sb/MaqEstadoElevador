de3954125ee6482a3ee4d939359b28785109f23e
André Braga <asbraga@hotmail.com>
2024-08-09 17:15:12 -0400

    Máquina simples para subir e descer

--- elevador.py
+++ elevador.py
@@ -1,12 +1,30 @@
 from pathlib import Path
 
+from transitions import Machine
+
 def v(obj):
     for x in dir(obj):
         if x[0]!='_':
             print(x)
 
+
+
 class Elevador:
     direcao=Path('./animacao/direcao.txt')
     tempo=Path('./animacao/tempo.txt')
+
+
+
+estados = ['parado','subindo','descendo']
+
+transicoes = [
+    {'source':'parado',        'trigger':'subir',      'dest':'subindo'        },
+    {'source':'parado',        'trigger':'descer',     'dest':'descendo'       },
+    {'source':'subindo',       'trigger':'chegou',     'dest':'parado'         },
+    {'source':'descendo',      'trigger':'chegou',     'dest':'parado'         },
+]
+
+elevador = Elevador()
+maquina  = Machine(model=elevador, states=estados, transitions=transicoes, initial='parado')
 
 pass
